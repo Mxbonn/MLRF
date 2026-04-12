04149caeff389eafe7f343ec901fbab4498712b5
Maxim Bonnaerens <maxim@bonnaerens.be>
2026-04-12 21:50:29 +0200

    site optimalizations

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mlrf.pages.dev',
   output: 'static',
+  integrations: [sitemap()],
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex]
